305af26e35fbe62a97fac9b90e121b7fa5651b88
Rafael <138421856+JavaRaf@users.noreply.github.com>
2026-08-17 22:30:35 -0300

    feat: send icon unificado nos botoes de comentario, composer e feed create

--- static/js/comments.js
+++ static/js/comments.js
@@ -325,7 +325,7 @@ document.addEventListener("click", async e => {
         form.innerHTML = `
             <div class="comment-reply-form-avatar"><img src="${avatarUrl()}" alt=""></div>
             <input type="text" placeholder="Responder @${username}..." autocomplete="off">
-            <button type="button" class="comment-reply-send">Enviar</button>`;
+            <button type="button" class="comment-reply-send"><svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor"><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg></button>`;
         commentEl.appendChild(form);
 
         const input = form.querySelector("input");
