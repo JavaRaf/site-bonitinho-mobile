e5cc875dff68a88d745ba8862f3377f90b14f578
Rafael <138421856+JavaRaf@users.noreply.github.com>
2026-08-17 22:32:01 -0300

    fix: send icon no botao de comentario do feed

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -479,7 +479,7 @@ function renderFeed() {
                 <div class="feed-comments-list"></div>
                 <form class="comment-form feed-comment-form">
                     <input type="text" placeholder="Adicione um comentário..." autocomplete="off">
-                    <button type="submit">Enviar</button>
+                    <button type="submit"><svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor"><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg></button>
                 </form>
             </div>
         </article>`;
